bd57e853d75465cf3c375e67037e1f63b5881393
Diego Cheng Gómez <diegocheng1973@gmail.com>
2019-11-16 01:47:07 -0600

    Timestamp calculates amount of ants in a certain time

--- src/Constants.java
+++ src/Constants.java
@@ -1,12 +1,23 @@
 
 
 //Clase enum por si se utiliza
-
+/*
 public enum Constants {
 
     SPEED_ANT,
     RANGE,
     SNAPSHOT_FRECUENCY,
 
+}*/
 
+public final class Constants {
+
+    private Constants() {
+        // restrict instantiation
+    }
+
+    public static final int SPEED_ANT = 1;
+    public static final double RANGE = 0.2;
+    public static final int TIMESTAMP_FRECUENCY = 10;
 }
+
--- src/Planning.java
+++ src/Planning.java
@@ -1,10 +1,6 @@
-import java.sql.Time;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
-import java.time.LocalTime;
-import java.sql.Time;
 import java.util.Date;
 
 public class Planning {
@@ -17,7 +13,7 @@ public class Planning {
         Date date = sdf.parse(this.initialTime); //Se hace parse de la fecha para guardarla
 
         for(int tree = 0; tree < pList_of_tree.size(); tree++){
-            Generate_times(pList_of_tree.get(tree), date);
+            Generate_times(pList_of_tree.get(tree), 0);
         }
 
     return pList_of_tree;  //Por ahora se coloco return solo para evitar error
@@ -25,8 +21,33 @@ public class Planning {
 
 
     //Hay que agregarle el tipo del return
-    public void Generate_times(Tree pTree, Date pDate){
+    public void Generate_times(Tree pTree, int time_seconds){
+        System.out.printf("Ground time: %d   Tree time: %d \n", calculate_ground_time(pTree), calculate_tree_time(pTree));
+        Timestamp timestamp = new Timestamp(time_seconds, calculate_ground_time(pTree) , calculate_tree_time(pTree), 100);
+        timestamp.calculate_states();
+        timestamp.print_states();
+    }
 
+    private int calculate_tree_time(Tree pTree){
+        float distance_value = 0;
+        float current = pTree.getTrunk();
+        for(int i = 0; i < pTree.getLevels(); i++){
+            distance_value += current;
+            current = current * pTree.getGrowth();
+        }
+        int tronco_a_hoja = Math.round(distance_value/Constants.SPEED_ANT);
+        return tronco_a_hoja;
+    }
+
+    private int calculate_ground_time(Tree pTree){
+        int hormiguero_a_arbol = Math.round(pTree.getDistance()/Constants.SPEED_ANT);
+        return hormiguero_a_arbol;
+    }
+
+    public static void main (String[] args){
+        Tree tree = new Tree(10,8,10, (float) 0.33,9);
+        Planning planning =  new Planning();
+        planning.Generate_times(tree,40);
     }
 
 }
