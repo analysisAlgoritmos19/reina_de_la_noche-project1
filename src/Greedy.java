8e4460ec3adc9a1fe72cf5128f262fb5f64ba19e
Esteban Salas <esalasestrada@gmail.com>
2019-11-19 22:17:40 -0600

    adaptacion del Tree del profe y probabilistic casi listo

--- src/Greedy.java
+++ src/Greedy.java
@@ -4,37 +4,24 @@ import java.util.TreeMap;
 
 public class Greedy {
 
-    private ArrayList<Tree> Sort_trees;
+    private ArrayList<TestTree> Sort_trees;
     private int Max_distance;
     private int Max_trunk;
-    private TreeMap<Double, Tree> map = new TreeMap<Double, Tree>();
+    private TreeMap<Double, TestTree> map = new TreeMap<Double, TestTree>();
 
-    public Greedy(ArrayList<Tree> pTrees){
-        for(Tree tree : pTrees){
+    public Greedy(ArrayList<TestTree> pTrees){
+        for(TestTree tree : pTrees){
             map.put(tree.getScore(), tree);
         }
-        printTrees();
     }
 
-    public void printTrees(){
-        System.out.println("__________________________________________________________________________-");
-        map.forEach((k, v) -> {
-            System.out.print("Key / Score: " + k + "\n");
-            System.out.print("Trunk size: " + v.getTrunk() + "\n");
-            System.out.print("Levels: " + v.getLevels() + "\n");
-            System.out.print("Growth: " + v.getGrowth() + "\n");
-            System.out.print("Leaf size: " + v.getLeaf_size() + "\n");
-            System.out.print("Amount leaves: " + v.getAmount_leaves() + "\n");
-            System.out.println("Distance" + v.getDistance() + "\n");
-        });
-    }
 
-    public int timeForTree(Tree tree){
+    public int timeForTree(TestTree tree){
         System.out.println();
         int ans = 0;
         ans += Max_distance;
         ans += Max_trunk;
-        double current = tree.getTrunk();
+        double current = tree.getLength();
         for(int i = 0; i < tree.getLevels(); i++){
             current = current * tree.getGrowth();
             ans += current;
